93c662a91a155f16c9032f2f51b1c4ce8ca722d2
muzai <muzairasim321@gmail.com>
2026-05-09 13:15:41 +0500

    Property.java

--- src/main/java/edu/project/model/tax/Property/Property.java
+++
@@ -1,4 +0,0 @@
-package edu.project.model.tax.Property;
-
-public class Property {
-}
---
+++ src/main/java/edu/project/model/tax/property/Property.java
@@ -0,0 +1,24 @@
+package edu.project.model.tax.property;
+
+import edu.project.model.tax.Taxable;
+
+public abstract class Property extends Taxable{
+    protected double urbanArea;
+    protected double agriArea;
+
+    public Property(double valuation, double urbanArea, double agriArea) {
+        super(valuation);
+        this.urbanArea = urbanArea;
+        this.agriArea = agriArea;
+    }
+
+    public void setUrbanArea(double urbanArea) {this.urbanArea = urbanArea;}
+    public void setAgriArea(double agriArea) {this.agriArea = agriArea;}
+
+    public double getUrbanArea() {return urbanArea;}
+    public double getAgriArea() {return agriArea;}
+
+    abstract public double calculateUrbanTax();
+    abstract public double calculateAgriTax();
+
+}
