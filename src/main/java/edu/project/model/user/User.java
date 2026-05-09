43cc02d42cff4286814a5440ad0fa196a984fb3f
muzai <muzairasim321@gmail.com>
2026-05-09 09:16:55 +0500

    designed User.java

--- src/main/java/edu/project/model/user/User.java
+++ src/main/java/edu/project/model/user/User.java
@@ -1,4 +1,21 @@
 package edu.project.model.user;
 
 public class User {
+    protected String password;
+    protected String id;
+    protected boolean loginStatus;
+
+    User(String password, String id) {
+        this.password = password;
+        this.id = id;
+        this.loginStatus = false;
+    }
+
+    public String getPassword() {return password;}
+    public String getId() {return id;}
+    public boolean isLoginStatus() {return loginStatus;}
+
+    public void setLoginStatus(boolean loginStatus) {this.loginStatus = loginStatus;}
+    public void setPassword(String password) {this.password = password;}
+    public void setId(String id) {this.id = id;}
 }
