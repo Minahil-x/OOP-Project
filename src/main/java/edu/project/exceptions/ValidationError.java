395820a707213203230886e5577d42ae699d553b
muzai <muzairasim321@gmail.com>
2026-05-09 09:36:44 +0500

    base UserManager.java added ValidationError.java

---
+++ src/main/java/edu/project/exceptions/ValidationError.java
@@ -0,0 +1,7 @@
+package edu.project.exceptions;
+
+public class ValidationError extends Exception{
+    public ValidationError(String message){
+        super(message);
+    }
+}
--- src/main/java/edu/project/manager/UserManager.java
+++ src/main/java/edu/project/manager/UserManager.java
@@ -1,4 +1,23 @@
 package edu.project.manager;
 
+import edu.project.exceptions.ValidationError;
+import edu.project.model.user.User;
+
+import java.util.ArrayList;
+
 public class UserManager {
+    private ArrayList<User> users;
+
+    public UserManager() {
+        users = new ArrayList<>();
+    }
+
+    public void register(User user) throws ValidationError {
+        for(User u : users){
+            if(u.getId().equals(user.getId())){
+                throw new ValidationError(user.getId() + " already exists");
+            }
+        }
+        users.add(user);
+    }
 }
