e99e080fc95d0dcc05ce55b1604cb8ade9fe5698
muzai <muzairasim321@gmail.com>
2026-05-09 13:20:19 +0500

    AJK.java

--- src/main/java/edu/project/model/tax/property/AJK.java
+++ src/main/java/edu/project/model/tax/property/AJK.java
@@ -1,4 +1,31 @@
 package edu.project.model.tax.property;
 
-public class AJK {
+import edu.project.model.user.TaxPayer;
+
+public class AJK extends Property{
+    public   AJK(double valuation, double urbanArea, double agriArea) {
+        super(valuation, urbanArea, agriArea);
+    }
+
+    //todo: methods
+    @Override
+    public double calculateUrbanTax(){
+        return 0;
+    }
+
+    @Override
+    public double calculateAgriTax(){
+        return 0;
+    }
+
+    @Override
+    public double calculateValuation(){
+        return 0;
+    }
+
+    @Override
+    public double calculateTax(TaxPayer payer){
+        return 0;
+    }
+
 }
