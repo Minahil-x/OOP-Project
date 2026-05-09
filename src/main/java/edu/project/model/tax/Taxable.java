02a9acc1e7e540d4729c1d5ba39d992df0b6dc61
muzai <muzairasim321@gmail.com>
2026-05-09 13:10:44 +0500

    Taxable.java

--- src/main/java/edu/project/model/tax/Taxable.java
+++ src/main/java/edu/project/model/tax/Taxable.java
@@ -1,4 +1,21 @@
 package edu.project.model.tax;
 
-public class Taxable {
+import edu.project.model.user.TaxPayer;
+
+import java.util.ArrayList;
+
+abstract public class Taxable {
+    protected double valuation;
+    protected ArrayList<String> tax;
+
+    public  Taxable(double valuation) {
+        this.valuation = valuation;
+    }
+
+    abstract public double calculateValuation();
+    abstract public double calculateTax(TaxPayer payer);
+
+    public ArrayList<String> getTaxes(){return tax;}
+
+    public void setTaxes(ArrayList<String> tax){this.tax = tax;}
 }
