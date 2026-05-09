c3058f717cdd0e2605c65116ad67ceec41efd027
muzai <muzairasim321@gmail.com>
2026-05-09 09:19:23 +0500

    base Admin.java

--- src/main/java/edu/project/model/user/Admin.java
+++ src/main/java/edu/project/model/user/Admin.java
@@ -1,4 +1,15 @@
 package edu.project.model.user;
 
-public class Admin {
+public class Admin extends User{
+    public Admin(String id, String password) {
+        super(id, password);
+    }
+
+    public String[][] getTable(){
+        return null;
+    }
+
+    public String[][] generateAnalytics(){
+        return null;
+    }
 }
