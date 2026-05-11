f1cb837d7dbffbb49f241dca8626320cbe3be910
muzai <muzairasim321@gmail.com>
2026-05-11 20:25:44 +0500

    Income.java logic fully implemented

--- src/main/java/edu/project/model/tax/income/Income.java
+++ src/main/java/edu/project/model/tax/income/Income.java
@@ -1,4 +1,34 @@
 package edu.project.model.tax.income;
 
-public class Income {
+import edu.project.model.tax.Taxable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class Income extends Taxable {
+    protected Map<String, Double> regionalIncome;
+    public Income(){
+        regionalIncome = new HashMap<String, Double>();
+        regionalIncome.put("punjab", 0.0);
+        regionalIncome.put("sindh", 0.0);
+        regionalIncome.put("baloch", 0.0);
+        regionalIncome.put("kpk", 0.0);
+        regionalIncome.put("ict", 0.0);
+        regionalIncome.put("ajk", 0.0);
+        regionalIncome.put("gb", 0.0);
+    }
+
+    public Map<String, Double> getRegionalIncome(){
+        return regionalIncome;
+    }
+
+    public void addRegionalIncome(String region, Double regionalIncomeValue){
+        this.regionalIncome.put(region.toLowerCase(), regionalIncomeValue);
+    }
+
+    public double getIncomeByRegion(String region){
+        return regionalIncome.getOrDefault(region.toLowerCase(), 0.0);
+    }
+
+    abstract public double calculateValuation();
 }
