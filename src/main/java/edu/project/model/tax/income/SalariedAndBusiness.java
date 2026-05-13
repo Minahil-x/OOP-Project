e6896355fb26aac3bb1fe34c5f9542c1205c2db9
Muhammad-Uzair-Asim <muzairasim323@gmail.com>
2026-05-13 09:55:44 +0500

    Update SalariedAndBusiness.java

--- src/main/java/edu/project/model/tax/income/SalariedAndBusiness.java
+++ src/main/java/edu/project/model/tax/income/SalariedAndBusiness.java
@@ -2,6 +2,7 @@ package edu.project.model.tax.income;
 
 public class SalariedAndBusiness extends Income{
   private double annualSalary;
+  private double taxD;
 
   public SalariedAndBusiness(double annualSalary){
     super();
@@ -11,4 +12,9 @@ public class SalariedAndBusiness extends Income{
   public void setAnnualSalary(double annualSalary){this.annualSalary = annualSalary;}
 
   public double getAnnualSalary(){return annualSalary;}
+
+  @Override
+  public double calculateTax(TaxPayer payer){
+    
+  }
 }
