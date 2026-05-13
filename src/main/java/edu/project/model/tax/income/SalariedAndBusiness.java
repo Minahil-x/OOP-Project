f734c37f0736f1d16e7908381cff0eb4c5cf1e3b
Muhammad-Uzair-Asim <muzairasim323@gmail.com>
2026-05-13 09:35:08 +0500

    Add annualSalary field and constructor to SalariedAndBusiness

--- src/main/java/edu/project/model/tax/income/SalariedAndBusiness.java
+++ src/main/java/edu/project/model/tax/income/SalariedAndBusiness.java
@@ -1,4 +1,9 @@
 package edu.project.model.tax.income;
 
 public class SalariedAndBusiness {
+  private double annualSalary;
+
+  public SalariedAndBusiness(double annualSalary){
+    super();
+    this.annualSalary = annualSalary;
 }
