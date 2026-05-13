38ce6bd24a421f79daf1214003a0ff661111569a
Muhammad-Uzair-Asim <muzairasim323@gmail.com>
2026-05-13 09:39:52 +0500

    Extend SalariedAndBusiness class from Income

--- src/main/java/edu/project/model/tax/income/SalariedAndBusiness.java
+++ src/main/java/edu/project/model/tax/income/SalariedAndBusiness.java
@@ -1,9 +1,14 @@
 package edu.project.model.tax.income;
 
-public class SalariedAndBusiness {
+public class SalariedAndBusiness extends Income{
   private double annualSalary;
 
   public SalariedAndBusiness(double annualSalary){
     super();
     this.annualSalary = annualSalary;
+  }
+
+  public void setAnnualSalary(double annualSalary){this.annualSalary = annualSalary;}
+
+  public double getAnnualSalary(){return annualSalary;}
 }
